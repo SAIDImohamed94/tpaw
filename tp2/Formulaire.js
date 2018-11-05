741c1fde2bb2222ac1461d3005bf2761748e756d
SAIDImohamed94 <44147067+SAIDImohamed94@users.noreply.github.com>
2018-11-05 20:20:26 +0100

    Add files via upload

---
+++ tp2/Formulaire.js
@@ -0,0 +1,34 @@
+function validation ( )
+{
+    document.getElementById("resultat").setAttribute("hidden",'hidden');
+    document.getElementById("error").setAttribute("hidden",'hidden');
+
+    if (document.getElementById("nom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le nom doit contenir au moins 5 caractère !";
+         document.getElementById("error").removeAttribute("hidden");
+    }  
+
+     
+     else if(document.getElementById("prenom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le prenom doit contenir au moins 5 caractère !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+      
+     else if(document.getElementById("Adresse").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "L'adresse doit contenir au moins 5 caractère !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+     
+     else if(document.getElementById("email").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le mail doit contenir au moins 5 caractère !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+     else {
+        document.getElementById("resultat").removeAttribute("hidden");
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value
+
+     }
+
+ }
+
+    
